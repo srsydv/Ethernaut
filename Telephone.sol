740f4269722538130a30c4e352ebfc66b4a5594d
Shrish Kumar <srsydv1802@gmail.com>
2023-09-05 12:31:57 +0530

    Update Telephone.sol

--- Telephone.sol
+++ Telephone.sol
@@ -1,1 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+import "./Telephone.sol";
 
+contract Call {
+    Telephone private immutable telephone;
+    constructor(address _telephone) {
+        telephone = Telephone(_telephone);
+    }
+
+    function callToChangeOwner(address newOwner) external {
+        telephone.changeOwner(newOwner);
+    }
+
+    function getOwner() external view returns(address) {
+        return telephone.owner();
+    }
+}
